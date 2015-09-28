c7885f514f45e4ab3a13d8c760415045ecb17ff3
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-09-28 16:10:17 +0100

    pp-126 Application skeleton
    This runs and connects to the DB on startup.

--- src/main/java/uk/gov/pay/publicauth/app/PublicAuthApp.java
+++ src/main/java/uk/gov/pay/publicauth/app/PublicAuthApp.java
@@ -1,9 +1,34 @@
 package uk.gov.pay.publicauth.app;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jdbi.bundles.DBIExceptionsBundle;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class PublicAuthApp extends Application<PublicAuthConfiguration> {
+
+    @Override
+    public void initialize(Bootstrap<PublicAuthConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor()
+                )
+        );
+        bootstrap.addBundle(new DBIExceptionsBundle());
+
+        bootstrap.addBundle(new MigrationsBundle<PublicAuthConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(PublicAuthConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+    }
+
+
     @Override
     public void run(PublicAuthConfiguration configuration, Environment environment) throws Exception {
         System.out.println("Hello world!");
