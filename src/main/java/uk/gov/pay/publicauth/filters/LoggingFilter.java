391dff2c0d9c0b4e5aa716af8ffdf83717b91cb7
Silvia Mandala <silvia.mandala@digital.cabinet-office.gov.uk>
2016-09-19 16:46:03 +0100

    PP-946 add requestId, time elapsed in all request logs

--- src/main/java/uk/gov/pay/publicauth/filters/LoggingFilter.java
+++ src/main/java/uk/gov/pay/publicauth/filters/LoggingFilter.java
@@ -1,31 +1,44 @@
 package uk.gov.pay.publicauth.filters;
 
+import com.google.common.base.Stopwatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
 
 public class LoggingFilter implements Filter {
 
+    public static final String HEADER_REQUEST_ID = "Request-Id";
     private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String requestURL = ((HttpServletRequest) servletRequest).getRequestURI();
+        Stopwatch stopwatch = Stopwatch.createStarted();
 
-        logger.info("Start - publicauth request - " + requestURL);
+        String requestURL = ((HttpServletRequest) servletRequest).getRequestURI();
+        String requestMethod = ((HttpServletRequest) servletRequest).getMethod();
+        Optional<String> requestIdMaybe = Optional.ofNullable(((HttpServletRequest) servletRequest).getHeader(HEADER_REQUEST_ID));
+        String requestId = requestIdMaybe.orElse("");
+
+        logger.info(format("[%s] - %s to %s began", requestId, requestMethod, requestURL));
         try {
             filterChain.doFilter(servletRequest, servletResponse);
-        } catch (Throwable throwable){
-            logger.error("Exception - publicauth request - "+ requestURL + " - exception - "+ throwable.getMessage(), throwable);
-        }
-        finally {
-            logger.info("End - publicauth request - " + requestURL);
+        } catch (Throwable throwable) {
+            logger.error("Exception - publicauth request - " + requestURL + " - exception - " + throwable.getMessage(), throwable);
+        } finally {
+            logger.info(format("[%s] - %s to %s ended - total time %dms", requestId, requestMethod, requestURL,
+                    stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+            stopwatch.stop();
         }
     }
 
