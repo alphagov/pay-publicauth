66e606859ffbd73feec9d9bb1a26a487a68acebd
Silvia Mandala <silvia.mandala@digital.cabinet-office.gov.uk>
2017-11-09 16:11:13 +0000

    PP-2888 Add token_type to tokens table
     - We are introducing a new payment type, direct debit. Currently we plan
       to associate a gateway_accoun to one of the two payment types.
     - This PR adds a new column to the tokens table, `token_type`, which can be of type
       `CREDIT_CARD` or `DIRECT_DEBIT`. The type has to be specified when storing (creating)
       the token.
     - For backwards compatibility, if there's no `token_type` field in the token creation request
       payload, the type is defaulting to `CREDIT_CARD`.

--- src/main/java/uk/gov/pay/publicauth/auth/Token.java
+++ src/main/java/uk/gov/pay/publicauth/auth/Token.java
@@ -1,17 +1,29 @@
 package uk.gov.pay.publicauth.auth;
 
+import uk.gov.pay.publicauth.model.TokenPaymentType;
+
 import java.security.Principal;
 
 public class Token implements Principal {
 
     private final String name;
+    private final TokenPaymentType tokenPaymentType;
 
     public Token(String name) {
+        this(name, TokenPaymentType.CREDIT_CARD);
+}
+
+    public Token(String name, TokenPaymentType tokenPaymentType) {
         this.name = name;
+        this.tokenPaymentType = tokenPaymentType;
     }
 
     @Override
     public String getName() {
         return name;
+    }
+
+    public TokenPaymentType getTokenPaymentType() {
+        return tokenPaymentType;
     }
 }
