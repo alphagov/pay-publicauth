b92e6fcf451cc22644cd1cba35ef9aa007e25941
Silvia Mandalà <sil.mandala@gmail.com>
2018-01-26 16:18:21 +0000

    PP-3061 Remove JodaTime in favour of Java 8 API
     with @alexbishop1

--- src/test/java/uk/gov/pay/publicauth/it/PublicAuthResourceITest.java
+++ src/test/java/uk/gov/pay/publicauth/it/PublicAuthResourceITest.java
@@ -6,15 +6,15 @@ import com.google.gson.Gson;
 import com.jayway.restassured.response.ValidatableResponse;
 import org.apache.commons.codec.digest.HmacUtils;
 import org.hamcrest.Matcher;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.ReadableInstant;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mindrot.jbcrypt.BCrypt;
-import uk.gov.pay.publicauth.resources.PublicAuthResource;
 import uk.gov.pay.publicauth.utils.DropwizardAppWithPostgresRule;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -31,11 +31,12 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
-import static org.joda.time.DateTimeZone.UTC;
 import static uk.gov.pay.publicauth.model.TokenPaymentType.CARD;
 import static uk.gov.pay.publicauth.model.TokenPaymentType.DIRECT_DEBIT;
 
 public class PublicAuthResourceITest {
+
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("dd MMM YYYY - HH:mm");
 
     private static final String SALT = "$2a$10$IhaXo6LIBhKIWOiGpbtPOu";
     private static final String BEARER_TOKEN = "testbearertoken";
@@ -53,8 +54,6 @@ public class PublicAuthResourceITest {
     private static final String TOKEN_HASH_COLUMN = "token_hash";
     private static final String CREATED_USER_NAME = "user-name";
     private static final String CREATED_USER_NAME2 = "user-name-2";
-
-    private DateTime now = DateTime.now();
 
     @Rule
     public DropwizardAppWithPostgresRule app = new DropwizardAppWithPostgresRule();
@@ -74,19 +73,19 @@ public class PublicAuthResourceITest {
         tokenResponse(apiKey)
                 .statusCode(200)
                 .body("account_id", is(ACCOUNT_ID));
-        DateTime lastUsed = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
-        DateTime currentTimeInDb = app.getDatabaseHelper().getCurrentTime();
-        assertThat(lastUsed, isCloseTo(currentTimeInDb));
+        ZonedDateTime lastUsed = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
+        assertThat(lastUsed, isCloseTo(ZonedDateTime.now(ZoneOffset.UTC)));
     }
 
     @Test
     public void respondWith401_whenAuthWithRevokedToken() throws Exception {
-        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, DateTime.now(), CREATED_USER_NAME);
+        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION,
+                ZonedDateTime.now(ZoneOffset.UTC), CREATED_USER_NAME);
         String apiKey = BEARER_TOKEN + encodedHmacValueOf(BEARER_TOKEN);
-        DateTime lastUsedPreAuth = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
+        ZonedDateTime lastUsedPreAuth = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
         tokenResponse(apiKey)
                 .statusCode(401);
-        DateTime lastUsedPostAuth = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
+        ZonedDateTime lastUsedPostAuth = app.getDatabaseHelper().getDateTimeColumn("last_used", ACCOUNT_ID);
 
         assertThat(lastUsedPreAuth, is(lastUsedPostAuth));
     }
@@ -175,13 +174,8 @@ public class PublicAuthResourceITest {
 
     @Test
     public void respondWith200_ifTokensHaveBeenIssuedForTheAccount() throws Exception {
-        DateTime inserted = app.getDatabaseHelper().getCurrentTime().toDateTime(UTC);
-        DateTime lastUsed = inserted.plusHours(1);
-        DateTime revoked = new DateTime(UTC)
-                .plusDays(1)
-                .withHourOfDay(00)
-                .withMinuteOfHour(20)
-                .withSecondOfMinute(0);
+        ZonedDateTime inserted = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime lastUsed = inserted.plusHours(1);
 
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, null, CREATED_USER_NAME, lastUsed);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN_2, TOKEN_LINK_2, ACCOUNT_ID, TOKEN_DESCRIPTION_2, null, CREATED_USER_NAME2, lastUsed, DIRECT_DEBIT);
@@ -200,8 +194,8 @@ public class PublicAuthResourceITest {
         assertThat(firstToken.containsKey("revoked"), is(false));
         assertThat(firstToken.get("created_by"), is(CREATED_USER_NAME2));
         assertThat(firstToken.get("token_type"), is(DIRECT_DEBIT.toString()));
-        assertThat(firstToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
-        assertThat(firstToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
+        assertThat(firstToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
 
         Map<String, String> secondToken = retrievedTokens.get(1);
         assertThat(secondToken.size(), is(6));
@@ -210,15 +204,15 @@ public class PublicAuthResourceITest {
         assertThat(secondToken.containsKey("revoked"), is(false));
         assertThat(secondToken.get("created_by"), is(CREATED_USER_NAME));
         assertThat(secondToken.get("token_type"), is(CARD.toString()));
-        assertThat(secondToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
-        assertThat(secondToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
+        assertThat(secondToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
+        assertThat(secondToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
     }
 
     @Test
     public void respondWith200_andRetrieveRevokedTokens() throws Exception {
-        DateTime inserted = app.getDatabaseHelper().getCurrentTime().toDateTime(DateTimeZone.UTC);
-        DateTime lastUsed = inserted.plusHours(1);
-        DateTime revoked = inserted.plusHours(2);
+        ZonedDateTime inserted = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime lastUsed = inserted.plusHours(1);
+        ZonedDateTime revoked = inserted.plusHours(2);
 
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, revoked, CREATED_USER_NAME, lastUsed);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN_2, TOKEN_LINK_2, ACCOUNT_ID, TOKEN_DESCRIPTION_2, null, CREATED_USER_NAME2, lastUsed);
@@ -233,18 +227,18 @@ public class PublicAuthResourceITest {
         Map<String, String> firstToken = retrievedTokens.get(0);
         assertThat(firstToken.get("token_link"), is(TOKEN_LINK));
         assertThat(firstToken.get("description"), is(TOKEN_DESCRIPTION));
-        assertThat(firstToken.get("revoked"), is(revoked.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("revoked"), is(revoked.format(DATE_TIME_FORMAT)));
         assertThat(firstToken.get("created_by"), is(CREATED_USER_NAME));
         assertThat(firstToken.get("token_type"), is(CARD.toString()));
-        assertThat(firstToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
-        assertThat(firstToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
+        assertThat(firstToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
     }
 
     @Test
     public void respondWith200_andRetrieveActiveTokens() throws Exception {
-        DateTime inserted = app.getDatabaseHelper().getCurrentTime().toDateTime(DateTimeZone.UTC);
-        DateTime lastUsed = inserted.plusHours(1);
-        DateTime revoked = inserted.plusHours(2);
+        ZonedDateTime inserted = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime lastUsed = inserted.plusHours(1);
+        ZonedDateTime revoked = inserted.plusHours(2);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, revoked, CREATED_USER_NAME, lastUsed);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN_2, TOKEN_LINK_2, ACCOUNT_ID, TOKEN_DESCRIPTION_2, null, CREATED_USER_NAME2, lastUsed);
 
@@ -260,15 +254,15 @@ public class PublicAuthResourceITest {
         assertThat(firstToken.containsKey("revoked"), is(false));
         assertThat(firstToken.get("created_by"), is(CREATED_USER_NAME2));
         assertThat(firstToken.get("token_type"), is(CARD.toString()));
-        assertThat(firstToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
-        assertThat(firstToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
+        assertThat(firstToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
     }
 
     @Test
     public void respondWith200_andRetrieveActiveTokensIfNoQueryParamIsSpecified() throws Exception {
-        DateTime inserted = app.getDatabaseHelper().getCurrentTime().toDateTime(UTC);
-        DateTime lastUsed = inserted.plusHours(1);
-        DateTime revoked = inserted.plusHours(2);
+        ZonedDateTime inserted = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime lastUsed = inserted.plusHours(1);
+        ZonedDateTime revoked = inserted.plusHours(2);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, revoked, CREATED_USER_NAME, lastUsed);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN_2, TOKEN_LINK_2, ACCOUNT_ID, TOKEN_DESCRIPTION_2, null, CREATED_USER_NAME2, lastUsed);
 
@@ -284,15 +278,15 @@ public class PublicAuthResourceITest {
         assertThat(firstToken.containsKey("revoked"), is(false));
         assertThat(firstToken.get("created_by"), is(CREATED_USER_NAME2));
         assertThat(firstToken.get("token_type"), is(CARD.toString()));
-        assertThat(firstToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
-        assertThat(firstToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
+        assertThat(firstToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
     }
 
     @Test
     public void respondWith200_andRetrieveActiveTokensIfUnknownQueryParamIsSpecified() throws Exception {
-        DateTime inserted = app.getDatabaseHelper().getCurrentTime().toDateTime(UTC);
-        DateTime lastUsed = inserted.plusHours(1);
-        DateTime revoked = inserted.plusHours(2);
+        ZonedDateTime inserted = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime lastUsed = inserted.plusHours(1);
+        ZonedDateTime revoked = inserted.plusHours(2);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, revoked, CREATED_USER_NAME, lastUsed);
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN_2, TOKEN_LINK_2, ACCOUNT_ID, TOKEN_DESCRIPTION_2, null, CREATED_USER_NAME2, lastUsed);
         List<Map<String, String>> retrievedTokens = getTokensFor(ACCOUNT_ID, "something")
@@ -307,8 +301,8 @@ public class PublicAuthResourceITest {
         assertThat(firstToken.containsKey("revoked"), is(false));
         assertThat(firstToken.get("created_by"), is(CREATED_USER_NAME2));
         assertThat(firstToken.get("token_type"), is(CARD.toString()));
-        assertThat(firstToken.get("last_used"), is(lastUsed.toString("dd MMM YYYY - HH:mm")));
-        assertThat(firstToken.get("issued_date"), is(inserted.toString("dd MMM YYYY - HH:mm")));
+        assertThat(firstToken.get("last_used"), is(lastUsed.format(DATE_TIME_FORMAT)));
+        assertThat(firstToken.get("issued_date"), is(inserted.format(DATE_TIME_FORMAT)));
     }
 
     @Test
@@ -370,14 +364,14 @@ public class PublicAuthResourceITest {
     @Test
     public void respondWith200_ifUpdatingDescriptionOfExistingToken() throws Exception {
         app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, CREATED_USER_NAME);
-        DateTime nowFromDB = app.getDatabaseHelper().getCurrentTime().toDateTime(UTC);
+        ZonedDateTime nowFromDB = ZonedDateTime.now(ZoneOffset.UTC);
 
         updateTokenDescription("{\"token_link\" : \"" + TOKEN_LINK + "\", \"description\" : \"" + TOKEN_DESCRIPTION_2 + "\"}")
                 .statusCode(200)
                 .body("token_link", is(TOKEN_LINK))
                 .body("description", is(TOKEN_DESCRIPTION_2))
-                .body("issued_date", is(nowFromDB.toString("dd MMM YYYY - HH:mm")))
-                .body("last_used", is(nowFromDB.toString("dd MMM YYYY - HH:mm")))
+                .body("issued_date", is(nowFromDB.format(DATE_TIME_FORMAT)))
+                .body("last_used", is(nowFromDB.format(DATE_TIME_FORMAT)))
                 .body("created_by", is(CREATED_USER_NAME))
                 .body("token_type", is(CARD.toString()));
 
@@ -397,7 +391,7 @@ public class PublicAuthResourceITest {
 
     @Test
     public void respondWith404_butDoNotUpdateRevokedTokens() throws Exception {
-        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, DateTime.now(), CREATED_USER_NAME);
+        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, ZonedDateTime.now(), CREATED_USER_NAME);
 
         updateTokenDescription("{\"token_link\" : \"" + TOKEN_LINK + "\", \"description\" : \"" + TOKEN_DESCRIPTION_2 + "\"}")
                 .statusCode(404)
@@ -437,7 +431,7 @@ public class PublicAuthResourceITest {
 
         revokeSingleToken(ACCOUNT_ID, "{\"token_link\" : \"" + TOKEN_LINK + "\"}")
                 .statusCode(200)
-                .body("revoked", is(now.toString("dd MMM YYYY")));
+                .body("revoked", is(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("dd MMM YYYY"))));
 
         Optional<String> revokedInDb = app.getDatabaseHelper().lookupColumnForTokenTable("revoked", "token_link", TOKEN_LINK);
         assertThat(revokedInDb.isPresent(), is(true));
@@ -460,7 +454,7 @@ public class PublicAuthResourceITest {
 
     @Test
     public void respondWith404_whenRevokingTokenAlreadyRevoked() throws Exception {
-        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, DateTime.now(), CREATED_USER_NAME);
+        app.getDatabaseHelper().insertAccount(HASHED_BEARER_TOKEN, TOKEN_LINK, ACCOUNT_ID, TOKEN_DESCRIPTION, ZonedDateTime.now(), CREATED_USER_NAME);
 
         revokeSingleToken(ACCOUNT_ID, "{\"token_link\" : \"" + TOKEN_LINK + "\"}")
                 .statusCode(404)
@@ -568,7 +562,7 @@ public class PublicAuthResourceITest {
                 .then();
     }
 
-    private Matcher<ReadableInstant> isCloseTo(DateTime now) {
+    private Matcher<ChronoZonedDateTime<?>> isCloseTo(ZonedDateTime now) {
         return both(greaterThan(now.minusSeconds(5))).and(lessThan(now.plusSeconds(5)));
     }
 
--- src/test/java/uk/gov/pay/publicauth/utils/DatabaseTestHelper.java
+++ src/test/java/uk/gov/pay/publicauth/utils/DatabaseTestHelper.java
@@ -1,11 +1,13 @@
 package uk.gov.pay.publicauth.utils;
 
-import io.dropwizard.jdbi.args.JodaDateTimeMapper;
+import io.dropwizard.jdbi.args.ZonedDateTimeMapper;
 import org.joda.time.DateTime;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.util.StringMapper;
 import uk.gov.pay.publicauth.model.TokenPaymentType;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TimeZone;
@@ -13,6 +15,7 @@ import java.util.TimeZone;
 import static uk.gov.pay.publicauth.model.TokenPaymentType.CARD;
 
 public class DatabaseTestHelper {
+
     private DBI jdbi;
 
     public DatabaseTestHelper(DBI jdbi) {
@@ -20,29 +23,26 @@ public class DatabaseTestHelper {
     }
 
     public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, String createdBy) {
-        insertAccount(tokenHash, randomTokenLink, accountId, description, null, createdBy, DateTime.now());
+        insertAccount(tokenHash, randomTokenLink, accountId, description, null, createdBy, ZonedDateTime.now(ZoneOffset.UTC));
     }
 
-    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, DateTime revoked, String createdBy) {
-        insertAccount(tokenHash, randomTokenLink, accountId, description, revoked, createdBy, DateTime.now());
+    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, ZonedDateTime revoked, String createdBy) {
+        insertAccount(tokenHash, randomTokenLink, accountId, description, revoked, createdBy, ZonedDateTime.now(ZoneOffset.UTC));
     }
 
-    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, DateTime revoked, String createdBy, DateTime lastUsed) {
+    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, ZonedDateTime revoked, String createdBy, ZonedDateTime lastUsed) {
         insertAccount(tokenHash, randomTokenLink, accountId, description, revoked, createdBy, lastUsed, CARD);
     }
 
-    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, DateTime revoked, String createdBy, DateTime lastUsed, TokenPaymentType tokenPaymentType) {
+    public void insertAccount(String tokenHash, String randomTokenLink, String accountId, String description, ZonedDateTime revoked, String createdBy, ZonedDateTime lastUsed, TokenPaymentType tokenPaymentType) {
         jdbi.withHandle(handle ->
         handle.insert("INSERT INTO tokens(token_hash, token_link, account_id, description, token_type, revoked, created_by, last_used) VALUES (?,?,?,?,?,(? at time zone 'utc'),?,(? at time zone 'utc'))",
-                tokenHash, randomTokenLink, accountId, description, tokenPaymentType, revoked, createdBy, lastUsed));
+                tokenHash, randomTokenLink, accountId, description, tokenPaymentType,
+                revoked, createdBy, lastUsed));
     }
 
-    public DateTime issueTimestampForAccount(String accountId) {
+    public ZonedDateTime issueTimestampForAccount(String accountId) {
         return getDateTimeColumn("issued", accountId);
-    }
-
-    public DateTime revokeTimestampForAccount(String accountId) {
-        return getDateTimeColumn("revoked", accountId);
     }
 
     public java.util.Optional<String> lookupColumnForTokenTable(String column, String idKey, String idValue) {
@@ -64,18 +64,11 @@ public class DatabaseTestHelper {
         return ret;
     }
 
-    public DateTime getDateTimeColumn(String column, String accountId) {
+    public ZonedDateTime getDateTimeColumn(String column, String accountId) {
         return jdbi.withHandle(handle ->
                 handle.createQuery("SELECT " + column + " FROM tokens WHERE account_id=:accountId")
                         .bind("accountId", accountId)
-                        .map(new JodaDateTimeMapper(Optional.of(TimeZone.getTimeZone("UTC"))))
-                        .first());
-    }
-
-    public DateTime getCurrentTime() {
-        return jdbi.withHandle(handle ->
-                handle.createQuery("SELECT (now() at time zone 'utc')")
-                        .map(new JodaDateTimeMapper(Optional.of(TimeZone.getTimeZone("UTC"))))
+                        .map(new ZonedDateTimeMapper(Optional.of(TimeZone.getTimeZone("UTC"))))
                         .first());
     }
 
