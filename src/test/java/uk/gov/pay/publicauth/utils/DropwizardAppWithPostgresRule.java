36833efa033582da3f668a0448236b0371d0ecc3
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-24 08:34:32 +0000

    Remove spurious blank line from DropwizardAppWithPostgresRule
    This is really a dummy commit to generate a release to test the pipeline, but
    we might as well make some kind of improvement whilst doing it.

--- src/test/java/uk/gov/pay/publicauth/utils/DropwizardAppWithPostgresRule.java
+++ src/test/java/uk/gov/pay/publicauth/utils/DropwizardAppWithPostgresRule.java
@@ -46,7 +46,6 @@ public class DropwizardAppWithPostgresRule implements TestRule {
 
                 restoreDropwizardsLogging();
 
-
                 base.evaluate();
             }
         }, description);
