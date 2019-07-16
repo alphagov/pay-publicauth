e37bf91c1b381e7f45cee7ce487570831fba6868
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-07-16 20:03:07 +0100

    REP-188: Manage database metrics in a more type-safe way
    Rather than a pair of typed Maps to collate database metrics, we can use Java's
    type system to encapsulate type-specific behaviour and (IMO) make the
    metrics-gathering code clearer & simpler.

--- src/main/java/uk/gov/pay/publicauth/service/DatabaseMetricsService.java
+++ src/main/java/uk/gov/pay/publicauth/service/DatabaseMetricsService.java
@@ -9,85 +9,66 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 
 import static java.lang.String.format;
 
 public class DatabaseMetricsService {
 
-    private static final Map<String, Long> longDatabaseStatsMap;
-    private static final Map<String, Double> doubleDatabaseStatsMap;
+    private final Set<PostgresMetric> metrics = Set.of(
+            new LongPostgresMetric("numbackends", 0L),
+            new LongPostgresMetric("xact_commit", 0L),
+            new LongPostgresMetric("xact_rollback", 0L),
+            new LongPostgresMetric("blks_read", 0L),
+            new LongPostgresMetric("blks_hit", 0L),
+            new LongPostgresMetric("tup_returned", 0L),
+            new LongPostgresMetric("tup_fetched", 0L),
+            new LongPostgresMetric("tup_inserted", 0L),
+            new LongPostgresMetric("tup_updated", 0L),
+            new LongPostgresMetric("tup_deleted", 0L),
+            new LongPostgresMetric("conflicts", 0L),
+            new LongPostgresMetric("temp_files", 0L),
+            new LongPostgresMetric("temp_bytes", 0L),
+            new LongPostgresMetric("deadlocks", 0L),
+            new DoublePostgresMetric("blk_read_time", 0.0),
+            new DoublePostgresMetric("blk_write_time", 0.0));
 
     private final String databaseName;
     private final DataSourceFactory dataSourceFactory;
     private Integer statsHealthy = 0;
 
-    static {
-        longDatabaseStatsMap = new HashMap<>();
-        longDatabaseStatsMap.put("numbackends", 0L);
-        longDatabaseStatsMap.put("xact_commit", 0L);
-        longDatabaseStatsMap.put("xact_rollback", 0L);
-        longDatabaseStatsMap.put("blks_read", 0L);
-        longDatabaseStatsMap.put("blks_hit", 0L);
-        longDatabaseStatsMap.put("tup_returned", 0L);
-        longDatabaseStatsMap.put("tup_fetched", 0L);
-        longDatabaseStatsMap.put("tup_inserted", 0L);
-        longDatabaseStatsMap.put("tup_updated", 0L);
-        longDatabaseStatsMap.put("tup_deleted", 0L);
-        longDatabaseStatsMap.put("conflicts", 0L);
-        longDatabaseStatsMap.put("temp_files", 0L);
-        longDatabaseStatsMap.put("temp_bytes", 0L);
-        longDatabaseStatsMap.put("deadlocks", 0L);
-        doubleDatabaseStatsMap = new HashMap<>();
-        doubleDatabaseStatsMap.put("blk_read_time", 0.0);
-        doubleDatabaseStatsMap.put("blk_write_time", 0.0);
-    }
-
     public DatabaseMetricsService(DataSourceFactory dataSourceFactory, MetricRegistry metricRegistry, String databaseName) {
         this.dataSourceFactory = dataSourceFactory;
         this.databaseName = databaseName;
-        initialiseMetrics(metricRegistry);
-    }
-
-    private void initialiseMetrics(MetricRegistry metricRegistry) {
-        for (String key : longDatabaseStatsMap.keySet()) {
-            metricRegistry.<Gauge<Long>>register(format("%sdb.%s", databaseName, key), () -> longDatabaseStatsMap.get(key));
-        }
-        for (String key : doubleDatabaseStatsMap.keySet()) {
-            metricRegistry.<Gauge<Double>>register(format("%sdb.%s", databaseName, key), () -> doubleDatabaseStatsMap.get(key));
-        }
+        metrics.forEach(metric -> metric.register(metricRegistry, format("%sdb.", databaseName)));
         metricRegistry.<Gauge<Integer>>register(format("%sdb.stats_healthy", databaseName), () -> statsHealthy);
     }
 
     public void updateMetricData() {
+        statsHealthy = fetchDatabaseMetrics() ? 1 : 0;
+    }
+
+    private boolean fetchDatabaseMetrics() {
         try (Connection connection = DriverManager.getConnection(
                 dataSourceFactory.getUrl(),
                 dataSourceFactory.getUser(),
-                dataSourceFactory.getPassword())) {
+                dataSourceFactory.getPassword());
+             PreparedStatement statement = connection.prepareStatement("select * from pg_stat_database where datname = ?")) {
             connection.setReadOnly(true);
-            try (PreparedStatement statement = connection.prepareStatement("select * from pg_stat_database where datname = ?")) {
-                statement.setString(1, databaseName);
-                if (statement.execute()) {
-                    try (ResultSet resultSet = statement.getResultSet()) {
-                        if (resultSet.next()) {
-                            for (String key : longDatabaseStatsMap.keySet()) {
-                                longDatabaseStatsMap.put(key, resultSet.getLong(key));
-                            }
-                            for (String key : doubleDatabaseStatsMap.keySet()) {
-                                doubleDatabaseStatsMap.put(key, resultSet.getDouble(key));
-                            }
-                            statsHealthy = 1;
-                        } else {
-                            statsHealthy = 0;
+            statement.setString(1, databaseName);
+            if (statement.execute()) {
+                try (ResultSet resultSet = statement.getResultSet()) {
+                    if (resultSet.next()) {
+                        for (PostgresMetric metric : metrics) {
+                            metric.setValueFromResultSet(resultSet);
                         }
+                        return true;
                     }
-                } else {
-                    statsHealthy = 0;
                 }
             }
-        } catch (SQLException e) {
-            statsHealthy = 0;
+        } catch (SQLException ignored) {
         }
+
+        return false;
     }
 }
