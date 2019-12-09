53cc449f1e665a7329877aaecdf0d4a50e1685c7
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-12-09 13:59:26 +0000

    PP-5894-Add-keys-to-MDC
    Description:
    - Adds GATEWAY_ACOUNT_ID to the MDC for logging
    - There is no Google Guice DI so we create the classes directly

---
+++ src/main/java/uk/gov/pay/publicauth/filters/LoggingMDCRequestFilter.java
@@ -0,0 +1,24 @@
+package uk.gov.pay.publicauth.filters;
+
+import org.slf4j.MDC;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.io.IOException;
+import java.util.Optional;
+
+import static uk.gov.pay.logging.LoggingKeys.GATEWAY_ACCOUNT_ID;
+
+public class LoggingMDCRequestFilter implements ContainerRequestFilter {
+
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        getPathParameterFromRequest("accountId", requestContext)
+                .ifPresent(accountId -> MDC.put(GATEWAY_ACCOUNT_ID, accountId));
+    }
+
+    private Optional<String> getPathParameterFromRequest(String parameterName, ContainerRequestContext requestContext) {
+        return Optional.ofNullable(requestContext.getUriInfo().getPathParameters().getFirst(parameterName));
+    }
+}
---
+++ src/main/java/uk/gov/pay/publicauth/filters/LoggingMDCResponseFilter.java
@@ -0,0 +1,17 @@
+package uk.gov.pay.publicauth.filters;
+
+import org.slf4j.MDC;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+import static uk.gov.pay.logging.LoggingKeys.GATEWAY_ACCOUNT_ID;
+
+public class LoggingMDCResponseFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MDC.remove(GATEWAY_ACCOUNT_ID);
+    }
+}
